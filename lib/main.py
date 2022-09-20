916ccd5c4bcb3ec485d1382e39155615a9bd2bf1
Jamie Moschella <jamie@boulderes.com>
2022-09-20 10:10:54 -0600

    No semicolons

--- lib/main.py
+++ lib/main.py
@@ -1,17 +1,17 @@
 import message
 
 def parseString(stdout):
-    stdout = stdout.toString();
+    stdout = stdout.toString()
 
-    messages = [*parseGcc(stdout), *parseLinker(stdout)];
+    messages = [*parseGcc(stdout), *parseLinker(stdout)]
 
-    return messages;
+    return messages
 
 
 def parseGcc(stdout):
-    messages = [];
-    match = None;
-    deepRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(\s+)\d*\s*[|]*\s*(.*)\s+[|]*\s*(~*\^~*)/gm;
+    messages = []
+    match = None
+    deepRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(\s+)\d*\s*[|]*\s*(.*)\s+[|]*\s*(~*\^~*)/gm
     #                  ^             ^     ^       ^             ^        ^    |            ^             ^
     #                  |             |     |       |             |        |    |            |             +- token marker
     #                  |             |     |       |             |        |    |            +- affected code
@@ -23,9 +23,9 @@ def parseGcc(stdout):
     #                  |             +- line
     #                  +- filename
     while ((match = deepRegex.exec(stdout))):
-        messages.push(new Message().fromGcc(match, stdout));
+        messages.push(new Message().fromGcc(match, stdout))
 
-    simpleRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(?!\s)/gm;
+    simpleRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(?!\s)/gm
     #                    ^             ^     ^       ^       ^     ^
     #                    |             |     |       |       |     |
     #                    |             |     |       |       |     +- whitespace before code
@@ -34,19 +34,19 @@ def parseGcc(stdout):
     #                    |             |     +- column
     #                    |             +- line
     #                    +- filename
-    match = None;
+    match = None
     while ((match = simpleRegex.exec(stdout))):
-        messages.push(new Message().fromGcc(match, stdout));
+        messages.push(new Message().fromGcc(match, stdout))
 
-    return messages;
+    return messages
 
 
 def parseLinker(stdout):
-    regex = /(.*):(\d+):\s(.*)\s(to|of)\s`(.*)'/g;
+    regex = /(.*):(\d+):\s(.*)\s(to|of)\s`(.*)'/g
 
-    messages = [];
-    match = None;
+    messages = []
+    match = None
     while ((match = regex.exec(stdout))):
-        messages.push(new Message().fromLinker(match, stdout));
+        messages.push(new Message().fromLinker(match, stdout))
 
-    return messages;
+    return messages
--- lib/message.py
+++ lib/message.py
@@ -1,83 +1,83 @@
 class Message:
     def __init__(self, filename, line, column, type, text, startIndex, endIndex):
-        self.filename: str = filename;
-        self.line: int = line;
-        self.column: int = column;
-        self.type: str = type;
+        self.filename: str = filename
+        self.line: int = line
+        self.column: int = column
+        self.type: str = type
         self.subtype = None
-        self.affectedSymbol = None;
-        self.text: str = text;
-        self.codeWhitespace = None;
-        self.code = None;
+        self.affectedSymbol = None
+        self.text: str = text
+        self.codeWhitespace = None
+        self.code = None
 
         # Length in characters of the token where the error has occurred
         self.tokenLength = None
 
         self.adjustedColumn = None
-        self.startIndex: int = startIndex;
-        self.endIndex: int = endIndex;
+        self.startIndex: int = startIndex
+        self.endIndex: int = endIndex
         self.parentFunction = None
         self.firstDefined = None
 
     def fromGcc(components, stdout):
-        self.filename = components[1];
-        self.line = parseInt(components[2]);
-        self.column = parseInt(components[3]);
-        self.type = components[4];
-        self.text = components[5];
-        self.codeWhitespace = components[6] if components[6] else '';
-        self.code = components[7] if components[7] else '';
+        self.filename = components[1]
+        self.line = parseInt(components[2])
+        self.column = parseInt(components[3])
+        self.type = components[4]
+        self.text = components[5]
+        self.codeWhitespace = components[6] if components[6] else ''
+        self.code = components[7] if components[7] else ''
         if (components[8]):
-            self.tokenLength = components[8].length;
+            self.tokenLength = components[8].length
 
-        self.adjustedColumn = self.column - self.codeWhitespace.length;
-        self.startIndex = stdout.indexOf(components[0]);
-        self.endIndex = self.startIndex + components[0].length;
-        self.parentFunction = self._lookbackFunction(stdout, self.startIndex);
+        self.adjustedColumn = self.column - self.codeWhitespace.length
+        self.startIndex = stdout.indexOf(components[0])
+        self.endIndex = self.startIndex + components[0].length
+        self.parentFunction = self._lookbackFunction(stdout, self.startIndex)
 
-        return self;
+        return self
 
     def fromLinker(components, stdout):
-        self.filename = components[1];
-        self.line = parseInt(components[2]);
-        self.column = 0;
-        self.type = 'error';
-        self.subtype = components[3];
-        self.affectedSymbol = components[5];
-        self.text = self.subtype + ' ' + components[4] + ' "' + self.affectedSymbol + '"';
+        self.filename = components[1]
+        self.line = parseInt(components[2])
+        self.column = 0
+        self.type = 'error'
+        self.subtype = components[3]
+        self.affectedSymbol = components[5]
+        self.text = self.subtype + ' ' + components[4] + ' "' + self.affectedSymbol + '"'
 
-        self.startIndex = stdout.indexOf(components[0]);
-        self.endIndex = self.startIndex + components[0].length;
-        self.parentFunction = self._lookbackFunction(stdout, self.startIndex);
+        self.startIndex = stdout.indexOf(components[0])
+        self.endIndex = self.startIndex + components[0].length
+        self.parentFunction = self._lookbackFunction(stdout, self.startIndex)
 
         if (self.subtype == 'multiple definition'):
-            self.firstDefined = self._lookupFirstDefinition(stdout, self.endIndex);
+            self.firstDefined = self._lookupFirstDefinition(stdout, self.endIndex)
 
-        return self;
+        return self
 
     def _matchAll(regex, input):
-        match = None;
-        matches = [];
+        match = None
+        matches = []
         while ((match = regex.exec(input))):
-            matches.push(match);
+            matches.push(match)
 
-        return matches;
+        return matches
 
     def _lookbackFunction(stdout, index):
-        regex = /In function\s(`|')(.*)'/g;
-        matches = self._matchAll(regex, stdout.slice(0, index));
+        regex = /In function\s(`|')(.*)'/g
+        matches = self._matchAll(regex, stdout.slice(0, index))
         if (matches.length):
-            return matches.slice(-1)[0][2];
+            return matches.slice(-1)[0][2]
 
-        return;
+        return
 
     def _lookupFirstDefinition(stdout, index):
-        regex = /:(.*):(\d+): first defined here/g;
+        regex = /:(.*):(\d+): first defined here/g
 
-        matches = self._matchAll(regex, stdout.slice(index));
+        matches = self._matchAll(regex, stdout.slice(index))
         if (matches.length):
             return {
                 "filename": matches[0][1],
                 "line": parseInt(matches[0][2])
-            };
-        return;
+            }
+        return
