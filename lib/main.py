e77cc32e65e628579f12be1e1bb6bf551bf35021
Jamie Moschella <jamie@boulderes.com>
2022-09-20 10:01:57 -0600

    Basic Python syntax

--- lib/main.py
+++ lib/main.py
@@ -1,56 +1,52 @@
-// import message
+import message
 
-function parseString(stdout) {
+def parseString(stdout):
     stdout = stdout.toString();
 
-    const messages = [...parseGcc(stdout), ...parseLinker(stdout)];
+    messages = [*parseGcc(stdout), *parseLinker(stdout)];
 
     return messages;
-}
 
-function parseGcc(stdout) {
-    const messages = [];
-    let match = null;
-    const deepRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(\s+)\d*\s*[|]*\s*(.*)\s+[|]*\s*(~*\^~*)/gm;
-    //                 ^             ^     ^       ^             ^        ^    |            ^             ^
-    //                 |             |     |       |             |        |    |            |             +- token marker
-    //                 |             |     |       |             |        |    |            +- affected code
-    //                 |             |     |       |             |        |    +- optional gcc 9.2 markup
-    //                 |             |     |       |             |        +- whitespace before code
-    //                 |             |     |       |             +- message text
-    //                 |             |     |       +- type (error|warning|note)
-    //                 |             |     +- column
-    //                 |             +- line
-    //                 +- filename
-    while ((match = deepRegex.exec(stdout))) {
-        messages.push(new Message().fromGcc(match, stdout));
-    }
 
-    const simpleRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(?!\s)/gm;
-    //                   ^             ^     ^       ^       ^     ^
-    //                   |             |     |       |       |     |
-    //                   |             |     |       |       |     +- whitespace before code
-    //                   |             |     |       |       +- message text
-    //                   |             |     |       +- type (error|warning|note)
-    //                   |             |     +- column
-    //                   |             +- line
-    //                   +- filename
-    match = null;
-    while ((match = simpleRegex.exec(stdout))) {
+def parseGcc(stdout):
+    messages = [];
+    match = None;
+    deepRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(\s+)\d*\s*[|]*\s*(.*)\s+[|]*\s*(~*\^~*)/gm;
+    #                  ^             ^     ^       ^             ^        ^    |            ^             ^
+    #                  |             |     |       |             |        |    |            |             +- token marker
+    #                  |             |     |       |             |        |    |            +- affected code
+    #                  |             |     |       |             |        |    +- optional gcc 9.2 markup
+    #                  |             |     |       |             |        +- whitespace before code
+    #                  |             |     |       |             +- message text
+    #                  |             |     |       +- type (error|warning|note)
+    #                  |             |     +- column
+    #                  |             +- line
+    #                  +- filename
+    while ((match = deepRegex.exec(stdout))):
         messages.push(new Message().fromGcc(match, stdout));
-    }
+
+    simpleRegex = /([^:^\r?\n]+):(\d+):(\d+):\s(\w+\s*\w*):\s(.+)\r?\n(?!\s)/gm;
+    #                    ^             ^     ^       ^       ^     ^
+    #                    |             |     |       |       |     |
+    #                    |             |     |       |       |     +- whitespace before code
+    #                    |             |     |       |       +- message text
+    #                    |             |     |       +- type (error|warning|note)
+    #                    |             |     +- column
+    #                    |             +- line
+    #                    +- filename
+    match = None;
+    while ((match = simpleRegex.exec(stdout))):
+        messages.push(new Message().fromGcc(match, stdout));
 
     return messages;
-}
 
-function parseLinker(stdout) {
-    const regex = /(.*):(\d+):\s(.*)\s(to|of)\s`(.*)'/g;
 
-    const messages = [];
-    let match = null;
-    while ((match = regex.exec(stdout))) {
+def parseLinker(stdout):
+    regex = /(.*):(\d+):\s(.*)\s(to|of)\s`(.*)'/g;
+
+    messages = [];
+    match = None;
+    while ((match = regex.exec(stdout))):
         messages.push(new Message().fromLinker(match, stdout));
-    }
 
     return messages;
-}
--- lib/message.py
+++ lib/message.py
@@ -1,15 +1,14 @@
-class Message {
-    fromGcc(components, stdout) {
+class Message:
+    def fromGcc(components, stdout):
         self.filename = components[1];
         self.line = parseInt(components[2]);
         self.column = parseInt(components[3]);
         self.type = components[4];
         self.text = components[5];
-        self.codeWhitespace = components[6] ? components[6] : '';
-        self.code = components[7] ? components[7] : '';
-        if (components[8]) {
+        self.codeWhitespace = components[6] if components[6] else '';
+        self.code = components[7] if components[7] else '';
+        if (components[8]):
             self.tokenLength = components[8].length;
-        }
 
         self.adjustedColumn = self.column - self.codeWhitespace.length;
         self.startIndex = stdout.indexOf(components[0]);
@@ -17,9 +16,8 @@ class Message {
         self.parentFunction = self._lookbackFunction(stdout, self.startIndex);
 
         return self;
-    }
 
-    fromLinker(components, stdout) {
+    def fromLinker(components, stdout):
         self.filename = components[1];
         self.line = parseInt(components[2]);
         self.column = 0;
@@ -32,41 +30,34 @@ class Message {
         self.endIndex = self.startIndex + components[0].length;
         self.parentFunction = self._lookbackFunction(stdout, self.startIndex);
 
-        if (self.subtype === 'multiple definition') {
+        if (self.subtype == 'multiple definition'):
             self.firstDefined = self._lookupFirstDefinition(stdout, self.endIndex);
-        }
 
         return self;
-    }
 
-    _matchAll(regex, input) {
-        let match = null;
-        const matches = [];
-        while ((match = regex.exec(input))) {
+    def _matchAll(regex, input):
+        match = None;
+        matches = [];
+        while ((match = regex.exec(input))):
             matches.push(match);
-        }
+
         return matches;
-    }
 
-    _lookbackFunction(stdout, index) {
-        const regex = /In function\s(`|')(.*)'/g;
-        const matches = self._matchAll(regex, stdout.slice(0, index));
-        if (matches.length) {
+    def _lookbackFunction(stdout, index):
+        regex = /In function\s(`|')(.*)'/g;
+        matches = self._matchAll(regex, stdout.slice(0, index));
+        if (matches.length):
             return matches.slice(-1)[0][2];
-        }
+
         return;
-    }
 
-    _lookupFirstDefinition(stdout, index) {
-        const regex = /:(.*):(\d+): first defined here/g;
+    def _lookupFirstDefinition(stdout, index):
+        regex = /:(.*):(\d+): first defined here/g;
 
-        const matches = self._matchAll(regex, stdout.slice(index));
-        if (matches.length) {
+        matches = self._matchAll(regex, stdout.slice(index));
+        if (matches.length):
             return {
-                filename: matches[0][1],
-                line: parseInt(matches[0][2])
+                "filename": matches[0][1],
+                "line": parseInt(matches[0][2])
             };
-        }
         return;
-    }
-}
